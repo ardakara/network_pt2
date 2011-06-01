06e79a41f2fe581349e72d5078b9e71786190530
Emily Stark <estark37@gmail.com>
2011-06-01 08:24:31 -0700

    check for tcp before counting packet

--- antiscanner.cpp
+++ antiscanner.cpp
@@ -33,37 +33,40 @@ int main(int argc, char* argv[]) {
   while ((packet = pcap_next(p, &hdr)) != NULL) {
     // read headers
     struct ip* ip_hdr = (struct ip*) (packet+14);
-    struct tcphdr* tcp_hdr = (struct tcphdr*) (packet+14+ip_hdr->ip_hl*4);
-    // convert IPs to strings
-    char source_ip[512];
-    char dest_ip[512];
-    inet_ntop(AF_INET, &(ip_hdr->ip_src), source_ip, 512);
-    inet_ntop(AF_INET, &(ip_hdr->ip_dst), dest_ip, 512);    
-    int syn = tcp_hdr->th_flags & TH_SYN;
-    int synack = (tcp_hdr->th_flags & TH_ACK) && syn;
+    if (ip_hdr->ip_p == 6) {
 
-    string key;
-    if (synack) {
-      key = string(dest_ip).append(" ").append(string(source_ip));
-    } else if (syn) {
-      key = string(source_ip).append(" ").append(string(dest_ip));
+      struct tcphdr* tcp_hdr = (struct tcphdr*) (packet+14+ip_hdr->ip_hl*4);
+      // convert IPs to strings
+      char source_ip[512];
+      char dest_ip[512];
+      inet_ntop(AF_INET, &(ip_hdr->ip_src), source_ip, 512);
+      inet_ntop(AF_INET, &(ip_hdr->ip_dst), dest_ip, 512);    
+      int syn = tcp_hdr->th_flags & TH_SYN;
+      int synack = (tcp_hdr->th_flags & TH_ACK) && syn;
+
+      string key;
+      if (synack) {
+	key = string(dest_ip).append(" ").append(string(source_ip));
+      } else if (syn) {
+	key = string(source_ip).append(" ").append(string(dest_ip));
+      }
+      struct counts c = count_map[key];
+      if (synack) {
+	c.syn_acks++;
+      } else if (syn) {
+	c.syns++;
+      }
+      count_map[key] = c;
     }
-    struct counts c = count_map[key];
-    if (synack) {
-      c.syn_acks++;
-    } else if (syn) {
-      c.syns++;
-    }
-    count_map[key] = c;
   }
 
   printf("Source-IP Destination-IP\n");
   map<string, struct counts>::iterator end = count_map.end();
   for (map<string, struct counts>::iterator it = count_map.begin(); it != end; ++it) {
-    printf("%s %d %d\n", it->first.c_str(), it->second.syns, it->second.syn_acks);
+    //printf("%s %d %d\n", it->first.c_str(), it->second.syns, it->second.syn_acks);
     struct counts c = it->second;
     if ((c.syn_acks == 0 && c.syns >= 3) || (c.syn_acks > 0 && c.syns / c.syn_acks > 3.0)) {
-      //      printf("%s\n", it->first.c_str());
+          printf("%s\n", it->first.c_str());
     }
   }
   
