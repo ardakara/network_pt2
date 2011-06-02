c7b441ded42c31f3afefa622446948dcf67af423
Emily Stark <estark37@gmail.com>
2011-06-01 21:41:49 -0700

    last minute changes to count SYNs and SYN/ACKs keyed by port and sequence number as well as IP

--- antiscanner.cpp
+++ antiscanner.cpp
@@ -6,6 +6,7 @@
 #include <arpa/inet.h>
 #include <map>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -13,6 +14,24 @@ struct counts {
   int syns;
   int syn_acks;
 };
+
+/* Returns a string with the following format:
+IP:PORT#SEQNO
+for example,
+127.0.0.1:80#1234
+Uses destination port if dest is true and source port otherwise.
+*/
+string get_ip_str(string ip, struct tcphdr* tcp_hdr, bool dest) {
+  string port;
+  stringstream ss;
+  if (!dest) {
+    ss << tcp_hdr->th_sport;
+  } else {
+    ss << tcp_hdr->th_dport;
+  }
+  ss << "#" << tcp_hdr->th_seq;
+  return ip.append(":").append(ss.str());
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 2) {
@@ -27,49 +46,80 @@ int main(int argc, char* argv[]) {
   struct pcap_pkthdr hdr;
   const u_char* packet = NULL;
 
-  // initialize map of src/dest IPs to syn/synack counts
+  /* Initialize a map (count_map) of raw syn and syn_ack counts.
+     This map has keys of the following form:
+    SRCIP:SRCPORT#SEQNO DESTIP:DESTPORT#SEQNO
+    It maps each key to a count of SYNs and SYN/ACKs that we've seen for this key.
+    ratios_map keeps track of the counts of SYNs and SYN/ACKs that we actually
+    use to compute the ratio, and it has keys of the form:
+    SRCIP DESTIP
+    We only increment the SYN/ACK count for a SRCIP/DESTIP pair once in ratios_map
+    per sequence number. This is so that if a destination resends a SYN/ACK because
+    it has not received an ACK, resent SYN/ACKs do not get counted in the ratio.
+   */
   map<string, struct counts> count_map;
+  map<string, struct counts> ratios_map;
 
   while ((packet = pcap_next(p, &hdr)) != NULL) {
     // read headers
     struct ip* ip_hdr = (struct ip*) (packet+14);
-    if (ip_hdr->ip_p == 6) {
 
+    if (ip_hdr->ip_p == 6) { // only use TCP packets
       struct tcphdr* tcp_hdr = (struct tcphdr*) (packet+14+ip_hdr->ip_hl*4);
+
       // convert IPs to strings
       char source_ip[512];
       char dest_ip[512];
       inet_ntop(AF_INET, &(ip_hdr->ip_src), source_ip, 512);
       inet_ntop(AF_INET, &(ip_hdr->ip_dst), dest_ip, 512);    
+
+      // is this packet a SYN or SYN/ACK or neither?
       int syn = tcp_hdr->th_flags & TH_SYN;
       int synack = (tcp_hdr->th_flags & TH_ACK) && syn;
 
-      string key;
+      // construct the keys to index into count_map and ratios_map
+      string count_key, ratio_key;
       if (synack) {
-	key = string(dest_ip).append(" ").append(string(source_ip));
+	count_key = get_ip_str(dest_ip, tcp_hdr, true).append(" ").append(get_ip_str(source_ip, tcp_hdr, false));
+	ratio_key = string(dest_ip).append(" ").append(string(source_ip));
       } else if (syn) {
-	key = string(source_ip).append(" ").append(string(dest_ip));
+	count_key = get_ip_str(source_ip, tcp_hdr, false).append(" ").append(get_ip_str(dest_ip, tcp_hdr, true));
+	ratio_key = string(source_ip).append(" ").append(string(dest_ip));
       }
-      struct counts c = count_map[key];
-      if (synack) {
-	c.syn_acks++;
-      } else if (syn) {
-	c.syns++;
+
+      // get the SYN and SYN/ACK counts for this key and increment if necessary
+      if (syn || synack) {
+	struct counts c = count_map[count_key];
+	if (synack) {
+	  c.syn_acks++;
+	} else if (syn) {
+	  c.syns++;
+	}
+	count_map[count_key] = c;
+
+	struct counts r = ratios_map[ratio_key];
+
+	// only increment SYN/ACK count in ratios_map once per sequence number
+	if (synack && c.syn_acks == 1) r.syn_acks += 1;
+	else if (syn) r.syns += c.syns;
+	ratios_map[ratio_key] = r;
+	
       }
-      count_map[key] = c;
     }
   }
 
+  // One more pass through ratios_map to print the output
   printf("Source-IP Destination-IP\n");
-  map<string, struct counts>::iterator end = count_map.end();
-  for (map<string, struct counts>::iterator it = count_map.begin(); it != end; ++it) {
-    //printf("%s %d %d\n", it->first.c_str(), it->second.syns, it->second.syn_acks);
-    struct counts c = it->second;
-    if ((c.syn_acks == 0 && c.syns >= 3) || (c.syn_acks > 0 && c.syns / c.syn_acks > 3.0)) {
-          printf("%s\n", it->first.c_str());
+  map<string, struct counts>::iterator end = ratios_map.end();
+  for (map<string, struct counts>::iterator it = ratios_map.begin(); it != end; ++it) {
+    struct counts r = ratios_map[it->first];
+    //printf("%s %d %d\n", it->first.c_str(), r.syns, r.syn_acks);
+    if (it->first.compare("0 0") != 0) {
+      if ((r.syn_acks == 0 && r.syns > 0) || (r.syn_acks > 0 && r.syns / r.syn_acks > 3.0)) {
+	printf("%s\n", it->first.c_str());
+      }
     }
   }
-  
 
   return 0;
 }
